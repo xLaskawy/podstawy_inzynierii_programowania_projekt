22b753b042127388eba6bb3e0f2ca10c739fdd47
Laskawy <laskawy@op.pl>
2022-01-25 20:51:20 +0100

    dodanie mavena i kawałek początku class

---
+++ src/main/java/inne/Skarga.java
@@ -0,0 +1,20 @@
+package inne;
+
+public class Skarga
+{
+    private String tresc;
+    private String imieSkladajacego, nazwiskoSkladajacego;
+    private String imieCelu, nazwiskoCelu;
+
+    public Skarga(String imieSkladajacego, String nazwiskoSkladajacego, String imieCelu, String nazwiskoCelu, String tresc)
+    {
+        this.imieSkladajacego = imieSkladajacego;
+        this.nazwiskoSkladajacego = nazwiskoSkladajacego;
+        this.imieCelu = imieCelu;
+        this.nazwiskoCelu = nazwiskoCelu;
+        this.tresc = tresc;
+    }
+
+
+
+}
---
+++ src/main/java/stanowiska/Stanowisko.java
@@ -0,0 +1,19 @@
+package stanowiska;
+
+public class Stanowisko
+{
+
+
+    public void wyswietlMenu()
+    {
+        System.out.println("Tutaj będą wypisane dostępne komendy dla tego stanowiska");
+        System.out.println("zglos_skarge");
+    }
+
+    public void zglosSkarge(String imieSkladajacego, String nazwiskoSkladajacego, String imieCelu, String nazwiskoCelu, String tresc)
+    {
+
+    }
+
+
+}
